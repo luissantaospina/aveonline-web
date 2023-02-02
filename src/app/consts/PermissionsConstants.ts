2373227c35acb914db4244543df2e2499cc61530
Luis <luisfernandosantaospina@gmail.com>
2023-02-02 17:42:55 -0500

    add permission to users

---
+++ src/app/consts/PermissionsConstants.ts
@@ -0,0 +1,8 @@
+export const PERMISSIONS_CONSTANTS = {
+  REVISAR_ÓRDENES_COMPRA: 'Revisar las órdenes de compra',
+  CARGAR_PRODUCTOS: 'Cargar los productos',
+  INGRESAR_ORDEN_COMPRA: 'Ingresar la orden de compra',
+  VER_ÓRDENES_COMPRA: 'Ver sus órdenes de compra'
+};
+
+export default PERMISSIONS_CONSTANTS;
