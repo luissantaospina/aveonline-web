289c1044c5213278e37e7c8d51bfbb05e2413c81
Luis <luisfernandosantaospina@gmail.com>
2023-02-01 21:14:44 -0500

    add delete role

--- src/app/home/role/role.component.ts
+++ src/app/home/role/role.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {RoleService} from "./role.service";
 import {Role} from "./role";
+import {User} from "../user/User";
 
 @Component({
   selector: 'app-role',
@@ -10,7 +11,18 @@ import {Role} from "./role";
 export class RoleComponent implements OnInit {
   constructor(private roleService: RoleService) { }
 
+  displayedColumns: string[] = ['name', 'actions'];
+
+  role: Role | undefined
+
   roles: Array<Role> = []
+
+  deleteRole(roleId: string): void {
+    this.roleService.deleteRole(roleId).subscribe(role => {
+      this.role = role
+    })
+    this.getRolesList()
+  }
 
   getRolesList(): void {
     this.roleService.getRoles().subscribe(roles => {
--- src/app/home/role/role.service.ts
+++ src/app/home/role/role.service.ts
@@ -3,6 +3,7 @@ import {environment} from "../../../environments/environment";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import { Role } from "./role";
+import {User} from "../user/User";
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,10 @@ export class RoleService {
   private apiUrl: string = environment.baseUrl
 
   constructor(private http: HttpClient) { }
+
+  deleteRole(id: string): Observable<Role> {
+    return this.http.delete<Role>(this.apiUrl + 'role/' + id)
+  }
 
   getRoles(): Observable<Role[]> {
     return this.http.get<Role[]>(this.apiUrl + 'roles')
