7d66d37d76fd972d35ca0d05eed2f03b6525be1e
Luis <luisfernandosantaospina@gmail.com>
2023-02-02 13:35:52 -0500

    redirect to login

--- src/app/guards/dashboard.guard.ts
+++ src/app/guards/dashboard.guard.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivateChild, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import {Router} from '@angular/router'
+import {LoginService} from "../login/login.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardGuard implements CanActivateChild {
+
+  constructor(private router:Router) { }
   canActivateChild(
     childRoute: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree
   {
-    return !!localStorage.getItem('token');
+    if (localStorage.getItem('token')) {
+      return true;
+    } else {
+      this.router.navigate(['login'])
+      return false;
+    }
   }
 }
